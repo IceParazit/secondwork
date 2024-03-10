85ffbb5c78545f2a6e46b6ec8beb097b3ea66d5e
IceParazit <34073415+IceParazit@users.noreply.github.com>
2024-03-10 18:19:57 +0300

    Create first_task.sh

---
+++ first_task.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+tmp_file=$(mktemp)
+
+grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b"  /home/lin-admin/test.txt | sort | uniq -c | sort -nr > "$tmp_file"
+
+while read -r count ip; do
+    if [ "$count" -gt 10 ]; then
+        echo "Блокировка IP $ip (попыток: $count)"
+        iptables -A INPUT -s "$ip" -j DROP
+    fi
+done < "$tmp_file"
+
+
+rm "$tmp_file"
